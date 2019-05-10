a4632ec495cd55ffdfb94ec00a86c8d241004e73
Aruna Benjamin <arunabenji29@gmail.com>
2019-05-10 11:21:22 -0600

    added routes and Navlinks

--- village/src/App.js
+++ village/src/App.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
-import {Route} from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 class App extends Component {
   constructor(props) {
@@ -16,22 +16,22 @@ class App extends Component {
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
 
-  componentDidMount(){
+  componentDidMount() {
     axios.get('http://localhost:3333/smurfs')
-    .then(res => {
-      console.log(res)
-      this.setState({
-        smurfs:res.data
+      .then(res => {
+        console.log(res)
+        this.setState({
+          smurfs: res.data
+        })
       })
-    })
-    .catch(err => {
-      console.log(err)
-    })
+      .catch(err => {
+        console.log(err)
+      })
   }
 
   addSmurf = (list) => {
     this.setState({
-      smurfs:list
+      smurfs: list
     })
     // this.props.history.push('/')
   }
@@ -39,8 +39,32 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <SmurfForm addSmurf={this.addSmurf}/>
-        <Smurfs smurfs={this.state.smurfs} />
+
+        <nav >
+          <div className="nav-links">
+            <NavLink to='/'>Home</NavLink>
+            <NavLink to='/smurf-form'>Add Smurf</NavLink>
+          </div>
+        </nav>
+
+        <Route path='/smurf-form'
+          render={props => (
+            <SmurfForm {...props}
+              smurfs={this.state.smurfs}
+              addSmurf={this.addSmurf}
+            />
+          )}
+        />
+
+        {/* <SmurfForm addSmurf={this.addSmurf}/> */}
+
+        <Route exact path='/'
+          render={props => (
+            <Smurfs
+              smurfs={this.state.smurfs}
+            />
+          )}
+        />
       </div>
     );
   }
